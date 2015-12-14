711bae016ac5ede4ef4af9cbe9436f532e747c93
oracle <oracle@oracle.freeflyer>
2015-12-14 12:18:23 +0300

    lesson-2

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 #include <ncurses.h>
+
+static void Draw(int x, int y, char* sym)
+{
+  mvprintw(y,x,sym);
+}
+
 int main(int argc, char *argv[])
 {
+  int x1=1;
+  int y1=3;
+  char* sym1="*";
+
+  int x2=4;
+  int y2=5;
+  char* sym2="#";
+  
+  
   initscr();
-  printw("Hello world");
+  curs_set(0);
+  Draw(x1,y1, sym1);
+  Draw(x2,y2, sym2);
   refresh();
   getch();
   endwin();

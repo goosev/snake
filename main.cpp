29058a7bf2f3d4be8eab877a4798394d3b13f069
oracle <oracle@oracle.freeflyer>
2015-12-14 13:11:25 +0300

    Sozdali klass Point. part3

--- main.cpp
+++ main.cpp
@@ -6,15 +6,19 @@ class Point
 public:
   int x,y;
   char* sym;
-  Point()
+
+  Point(int _x, int _y, char* _sym)
   {
-  }
+    x=_x;
+    y=_y;
+    sym=_sym;
+  };
   
-  void Point::Draw()
+  void Draw()
   {
     move(y,x);
     printw(sym);
-  }
+  };
 
 };
         
@@ -22,21 +26,18 @@ public:
 int main(int argc, char *argv[])
 {
 
-  Point *p1=new Point();
-  p1->x=1;
-  p1->y=3;
-  p1->sym="*";
-
+  Point *p1=new Point(1,3,"*");
+/*
   Point *p2=new Point();
   p2->x=4;
   p2->y=5;
   p2->sym="#";
-  
+*/
   
   initscr();
   curs_set(0);
   p1->Draw();
-  p2->Draw();
+  //p2->Draw();
   refresh();
   getch();
   endwin();
